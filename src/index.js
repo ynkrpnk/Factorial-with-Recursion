58d5767c533444a469f813c7d9b60dc46367042d
ynkrpnk <ankarpenko35@gmail.com>
2026-04-23 12:29:50 +0300

    added checks

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-'use strict';
+"use strict";
 
-function factorial(num){
-if (num === 0 || num === 1) {
+function factorial(num) {
+  if (typeof num !== "number" || Number.isNaN(num) || num < 0) {
+    return null;
+  }
+  if (num === 0 || num === 1) {
     return 1;
   }
   return num * factorial(num - 1);
 }
 
 console.log(factorial(6));
-
